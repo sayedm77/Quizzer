ec1da1019fc5e71b3577946078f1aaed6cdf132d
Sayedmans <sayedmansoo123@gmai.com>
2024-01-16 00:05:47 +0200

    Initial commit

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,17 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by elsayed mansour mahfouz on 11/8/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+struct  Question {
+    let text : String
+    let answer : String
+    init(q:String , a :String) {
+        text = q
+        answer = a
+    }
+}
